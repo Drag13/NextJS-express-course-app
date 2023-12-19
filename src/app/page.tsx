b3f25245cfef72ddf26c40c7c5095de9665ba954
drag13 <no-questions@github.noreply.com>
2023-12-19 14:37:34 +0200

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,15 @@ import course from './course.json'
 export default function Home() {
   return (
     <main className={styles.main}>
-
       <ul>
-      {course.lessons.map((lesson)=> {
-        return  <li key={lesson.name}>
-
-        <h2> {lesson.title} </h2>          
-
-  <p>
-    {lesson.shortSummary}
-  </p>
-        </li>
-      })}
+        {course.lessons.map((lesson) => {
+          return <li key={lesson.name}>
+            <h2> {lesson.title} </h2>
+            <p>
+              {lesson.shortSummary}
+            </p>
+          </li>
+        })}
       </ul>
     </main>
   )
